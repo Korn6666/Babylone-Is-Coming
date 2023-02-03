244751f2941e112dc68382003f3f8d275f012010
Korn6666 <simonmanes.sm@gmail.com>
2023-02-03 22:11:42 +0100

    TestCommit2

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    public float health;
+
+    //public HealthBar healthBar;
+
+    void Start()
+    {
+        health = maxHealth;
+        //healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        //healthBar.SetHealth(health);
+
+        if (health <= 0f) //condition de destruction 
+        {
+            Destroy(gameObject); //Détruis l'objet
+        }
+
+        if(health > maxHealth) //Empêcher 
+        {
+            health = maxHealth;
+        }
+    }
+}
