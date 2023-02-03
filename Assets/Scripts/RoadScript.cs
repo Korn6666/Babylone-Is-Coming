85bb0b11553e57eaca8765a4647c325c85b10024
Korn6666 <simonmanes.sm@gmail.com>
2023-02-04 00:13:52 +0100

    merde

---
+++ Assets/Scripts/RoadScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadScript : MonoBehaviour
+{
+
+    private Transform[] RoadSprites;
+    void Start()
+    {
+        RoadSprites = gameObject.GetComponentsInChildren<Transform>();
+        // foreach(Transform RoadSprite in RoadSprites)
+        // {
+        //     Debug.Log(RoadSprite.gameObject.name);
+        // }
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/RoadTileScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadTileScript : MonoBehaviour
+{
+
+    
+    private void OnTriggerEnter( Collider Entity)
+    {
+        // if (Entity.tag == "Enemy")
+        // {
+        //     //Entity.GetComponent<EnemyMovement>().MoveNextTile = true;
+        // }
+        Debug.Log("ya");
+    }
+}
