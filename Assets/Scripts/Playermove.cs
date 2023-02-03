dc5b921b4ed2122361a4867cc990cf431b7f83bf
Karleuh <kjonquais@gmail.com>
2023-02-03 23:54:16 +0100

    PREMIER PUSH OMG

---
+++ Assets/Scripts/Playermove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+
+    private float horiz;
+    private float verti;
+    public float speed;
+
+    void Update()
+    {
+        horiz = Input.GetAxis("Horizontal");
+        verti = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horiz,verti,0) * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/recolte.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recolte : MonoBehaviour
+{
+
+    public GameObject Jardinier;
+    public GameObject WaveManager;
+    public int grammes;
+    public int tourSansRecolte;
+
+
+    // Update is called once per frame
+    void Grow()
+    {
+        tourSansRecolte +=1;
+        grammes+= 10*tourSansRecolte;
+    }
+
+    void Jardined()
+        {
+            grammes=0;
+        }
+
+
+}
