93555090a216bec02cf3d3d4c15d492443666100
Korn6666 <simonmanes.sm@gmail.com>
2023-02-05 06:10:31 +0100

    caca

--- Assets/Scripts/Playermove.cs
+++ Assets/Scripts/Playermove.cs
@@ -14,6 +14,7 @@ public class Playermove : MonoBehaviour
     [SerializeField] private Animator playerAnimator;
     public GameObject CanvasBuild;
     public bool isMoving;
+    private bool atHome;
 
     void Start()
     {
@@ -27,18 +28,20 @@ public class Playermove : MonoBehaviour
             BaseDistance = (BaseTransform.position - transform.position).magnitude;
             if ( BaseDistance > 1 )
             {
+                atHome = false;
                 playerAnimator.SetBool("isMoving", true);
                 BaseDirection = (BaseTransform.position - transform.position).normalized;
                 transform.Translate( BaseDirection * Time.deltaTime * speed);
             }
             else 
             {
+                atHome = true;
                 playerAnimator.SetBool("isMoving", false);
             }
-
         }
         else 
         {
+            atHome = false;
             horiz = Input.GetAxis("Horizontal");
             verti = Input.GetAxis("Vertical");
 
@@ -61,21 +64,30 @@ public class Playermove : MonoBehaviour
                 //Canvas apparait si immobile
                 CanvasBuild.SetActive(true);
             }
+        }
 
-        void ResetCanvas()
+        if (atHome)
         {
-                CanvasBuild.transform.GetChild(0).gameObject.SetActive(false);
-                CanvasBuild.transform.GetChild(1).gameObject.SetActive(false);
-                CanvasBuild.transform.GetChild(2).gameObject.SetActive(false);
-                CanvasBuild.transform.GetChild(3).gameObject.SetActive(false);
-                CanvasBuild.transform.GetChild(4).gameObject.SetActive(false);
-                CanvasBuild.transform.GetChild(5).gameObject.SetActive(false);
-                CanvasBuild.transform.GetChild(6).gameObject.SetActive(false);
-                CanvasBuild.transform.GetChild(7).gameObject.SetActive(false);
+            Debug.Log("atHome");
+            BaseTransform.GetChild(0).gameObject.SetActive(false);
+            BaseTransform.GetChild(1).gameObject.SetActive(true);
+        }else 
+        {
+            Debug.Log("NotatHome");
+            BaseTransform.GetChild(0).gameObject.SetActive(true);
+            BaseTransform.GetChild(1).gameObject.SetActive(false);
         }
+    }
 
-
-        }
-
+    void ResetCanvas()
+    {
+            CanvasBuild.transform.GetChild(0).gameObject.SetActive(false);
+            CanvasBuild.transform.GetChild(1).gameObject.SetActive(false);
+            CanvasBuild.transform.GetChild(2).gameObject.SetActive(false);
+            CanvasBuild.transform.GetChild(3).gameObject.SetActive(false);
+            CanvasBuild.transform.GetChild(4).gameObject.SetActive(false);
+            CanvasBuild.transform.GetChild(5).gameObject.SetActive(false);
+            CanvasBuild.transform.GetChild(6).gameObject.SetActive(false);
+            CanvasBuild.transform.GetChild(7).gameObject.SetActive(false);
     }
 }
