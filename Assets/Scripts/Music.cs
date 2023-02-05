3be98373a6a3c307e34c94890bd184e9eb945f09
Korn6666 <simonmanes.sm@gmail.com>
2023-02-05 12:41:32 +0100

    Music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private static GameObject music;
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+
+        if (music == null)
+        {
+            music = transform.gameObject;
+        }
+        else
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
+
