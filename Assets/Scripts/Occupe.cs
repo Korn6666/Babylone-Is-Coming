449750a627b3ee4694a891ca8807e4bab3b7e956
Korn6666 <simonmanes.sm@gmail.com>
2023-02-05 04:08:53 +0100

    highlights

--- Assets/Scripts/Occupe.cs
+++ Assets/Scripts/Occupe.cs
@@ -5,4 +5,26 @@ using UnityEngine;
 public class Occupe : MonoBehaviour
 {
     public bool boolOccupe = false;
+
+    // void OnTriggerExit ( Collider tile)
+    // {
+    //     if (tile.gameObject.tag == "Plots")
+    //     {
+    //         tile.transform.GetChild(0).gameObject.SetActive(false);
+    //     }
+    // }
+
+    // void OnTriggerEnter(Collider tile)
+    // {
+    //     if (tile.gameObject.tag=="Plants")
+    //         {
+    //             currentTile=tile.gameObject;
+    //         }
+    //     if (tile.gameObject.tag == "Plots")
+    //     {
+    //             currentTile=tile.gameObject;
+    //             tile.transform.GetChild(0).gameObject.SetActive(true);
+    //     }
+
+    // }
 }
